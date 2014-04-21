1653312e75ef3c08725f5ebcf17b4fb07f52380c
Stephen Turner <vustephen@gmail.com>
2014-04-21 13:12:12 -0400

    fixed n documentation

--- R/n.R
+++ R/n.R
@@ -3,9 +3,9 @@
 #' Given data.frame df, return the `names` of that data.frame
 #' in a "long" one-column numbered format.
 #' 
-#' @param df A data.frame object \{code df}
+#' @param df A data.frame object \code{df}
 #' 
-#' @return output A single column numbered list of data.frame names
+#' @return A single column numbered list of data.frame names
 #' 
 #' @keywords keywords
 #' 
@@ -14,4 +14,6 @@
 #' @examples
 #' n(iris)
 
-n <- function(df) matrix(names(df)) 
+n <- function(df) {
+    matrix(names(df))
+}
