818259a22d5fb69e8babde439d88351e20ca245a
Stephen Turner <vustephen@gmail.com>
2014-04-21 16:20:23 -0400

    addRawFC, mergett, rownames_to_symprobe

---
+++ R/addRawFC.R
@@ -0,0 +1,38 @@
+#' Add more FCs to topTable
+#' 
+#' Takes a limma topTable as input, and adds the absolute value of the log2 FC, and the raw FC (2^logFC).
+#' 
+#' @param tt A topTable
+#'   
+#' @return A new topTable with absLogFC and rawFC
+#'   
+#' @keywords keywords
+#'   
+#' @export
+#' 
+#' @examples
+#' fit <- lmFit(eset,design)
+#' fit <- contrasts.fit(fit, contrast.matrix)
+#' fit <- eBayes(fit)
+#' tt <- topTable(fit, coef="mycontrast", number=nrow(fit))
+#' tt <- addRawFC(tt)
+ 
+addRawFC <- function(tt) {
+    if (class(tt)!="data.frame") stop("Doesn't look like a data.frame")
+    if (!all(c("logFC", "AveExpr", "t", "P.Value", "adj.P.Val", "B") == names(tt)[(ncol(tt)-5):ncol(tt)])) stop("Doesn't look like a topTable")
+    tt <- tt[order(tt$P.Value), ]
+    ttmeta <- tt[ ,1:(ncol(tt)-6)]
+    logFC <- tt$logFC
+    absLogFC <- abs(logFC)
+    rawFC <- 2^logFC
+    tt2 <- cbind(ttmeta,
+                 logFC,
+                 absLogFC,
+                 rawFC,
+                 subset(tt, select=c(AveExpr, t, P.Value, adj.P.Val)))
+    if (all(tt$logFC==tt2$logFC)) {
+        return(tt2)
+    }   else {
+        stop("Something went horribly wrong. Check that tt$LogFC==tt2$LogFC")
+    }
+}
---
+++ R/mergett.R
@@ -0,0 +1,42 @@
+#' Merge topTable with expression values
+#' 
+#' Merges topTable output with normalized expression values from the
+#' ExpressionSet. Note: run \code{addRawFC(tt)} before merging with
+#' ExpressionSet values.
+#' 
+#' @param tt A topTable
+#' @param eset An ExpressionSet object from which tt derives
+#'   
+#' @return An expanded topTable with gene expression values merged in, sorted by p-value.
+#'   
+#' @keywords keywords
+#'   
+#' @export
+#' 
+#' @examples
+#' fit <- lmFit(eset,design)
+#' fit <- contrasts.fit(fit, contrast.matrix)
+#' fit <- eBayes(fit)
+#' tt <- topTable(fit, coef="mycontrast", number=nrow(fit))
+#' tt <- addRawFC(tt)
+#' tt <- mergett(tt, eset)
+
+mergett <- function(tt, eset) {
+    if (class(tt)!="data.frame") stop("tt should be a data.frame")
+    if (class(eset)!="ExpressionSet") stop("eset should be an ExpressionSet")
+    exprset <- data.frame(exprs(eset))
+    exprset$ID <- rownames(exprset)
+    rownames(exprset) <- NULL
+    message(paste("nrow(tt):", nrow(tt)))
+    message(paste("nrow(exprset):", nrow(exprset)))
+    if (nrow(tt)==nrow(exprset)) {
+        # Merge with intensities
+        newtt <- merge(tt, exprset)
+    } else {
+        stop("nrow(tt) != nrow(exprset)")
+    }
+    message(paste("nrow(newtt):", nrow(newtt)))
+    newtt <- newtt[order(newtt$P.Val), ] #reorder by p-value
+    rownames(newtt) <- NULL
+    newtt
+}
