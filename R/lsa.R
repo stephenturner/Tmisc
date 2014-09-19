9059b1ddf0a6fb734cabb6e31d3578bb7b3a116e
Stephen Turner <vustephen@gmail.com>
2014-09-19 15:31:57 -0400

    changed lsa to use Dirk Eddelbuettels method

--- R/lsa.R
+++ R/lsa.R
@@ -1,21 +1,48 @@
-#' List objects and classes
+#' Improved list of objects
 #' 
-#' List all the objects and their classes in the global environment.
+#' Improved list of objects.  Sorts by size by default.  This was shamelessly 
+#' stolen from \url{http://stackoverflow.com/q/1358003/654296}
 #' 
-#' @return A data frame with all objects and their class.
-#' 
+#' @author Dirk Eddelbuettel, Tony Breyal
 #' @keywords keywords
-#' 
+#'   
+#' @param pos numeric. Position in the stack
+#' @param pattern Regex to filter the objects by
+#' @param order.by character. Either 'Type', 'Size', 'PrettySize', 'Rows', or 
+#'   'Columns'. This will dictate how the output is ordered
+#' @param decreasing logical. Should the output be displayed in decreasing order
+#' @param head logical. Use head on the output?
+#' @param n numeric. Number of objects to display is head is TRUE
+#'   
 #' @export
 #' 
 #' @examples
-#' # lsa()
+#' \dontrun{
+#' a <- rnorm(100000)
+#' b <- matrix(1, 1000, 100)
+#' lsa()
+#' }
 
-lsa <- function() {
-    obj_type <- function(x) class(get(x, envir = .GlobalEnv)) # define environment
-    foo = data.frame(sapply(ls(envir = .GlobalEnv), obj_type))
-    foo$object_name = rownames(foo)
-    names(foo)[1] = "class"
-    names(foo)[2] = "object"
-    return(unrowname(foo))
+lsa <- function (pos = 1, pattern, order.by = "Size",
+                  decreasing=TRUE, head=TRUE, n=10) {
+    napply <- function(names, fn) sapply(names, function(x)
+        fn(get(x, pos = pos)))
+    names <- ls(pos = pos, pattern = pattern)
+    obj.class <- napply(names, function(x) as.character(class(x))[1])
+    obj.mode <- napply(names, mode)
+    obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
+    obj.prettysize <- napply(names, function(x) {
+        capture.output(print(object.size(x), units = "auto")) })
+    obj.size <- napply(names, object.size)
+    obj.dim <- t(napply(names, function(x)
+        as.numeric(dim(x))[1:2]))
+    vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
+    obj.dim[vec, 1] <- napply(names, length)[vec]
+    out <- data.frame(obj.type, obj.size, obj.prettysize, obj.dim)
+    names(out) <- c("Type", "Size", "PrettySize", "Rows", "Columns")
+    if (!missing(order.by))
+        out <- out[order(out[[order.by]], decreasing=decreasing), ]
+    if (head)
+        out <- head(out, n)
+    out
 }
