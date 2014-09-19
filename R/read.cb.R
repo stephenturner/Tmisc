9fc80c12d7ec055d80a223b05b4da90fcefd8b5c
Stephen Turner <vustephen@gmail.com>
2014-09-19 16:10:49 -0400

    more documentation, added unfactor and read.cb

---
+++ R/read.cb.R
@@ -0,0 +1,24 @@
+#' Read from the clipboard
+#' 
+#' Read tabular data from the clipboard
+#' 
+#' @author Stephen Turner
+#' @keywords keywords
+#' 
+#' @param ... Arguments to be passed to \code{read.table}
+#' 
+#' @return A data.frame
+#' 
+#' @export
+#' 
+#' @examples
+#' \dontrun{
+#' # To read CSV data with a header from the clipboard:
+#' read.cb(header=TRUE, sep=',')
+#' }
+
+read.cb <- function(...) {
+    ismac <- Sys.info()[1]=="Darwin"
+    if (!ismac) read.table(file="clipboard", ...)
+    else read.table(pipe("pbpaste"), ...)
+}
