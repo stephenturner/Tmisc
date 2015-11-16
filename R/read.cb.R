b6ff4a00f4f8acef5ab8e558dd22f7d25dfa4e29
Stephen Turner <vustephen@gmail.com>
2015-11-16 13:34:20 -0500

    read.cb now makes tbl_df if dplyr is on search path

--- R/read.cb.R
+++ R/read.cb.R
@@ -5,7 +5,8 @@
 #' @author Stephen Turner
 #' @keywords keywords
 #' 
-#' @param ... Arguments to be passed to \code{read.table}
+#' @param header A logical value indicating whether the file contains the names of the variables as its first line. Overrides the default \code{header=FALSE} option in \code{read.table()}.
+#' @param ... Further arguments to be passed to \code{read.table}
 #' 
 #' @return A data.frame
 #' @import utils
@@ -17,8 +18,11 @@
 #' }
 #' 
 #' @export
-read.cb <- function(...) {
-    ismac <- Sys.info()[1]=="Darwin"
-    if (!ismac) read.table(file="clipboard", ...)
-    else read.table(pipe("pbpaste"), ...)
+read.cb <- function(header=TRUE, ...) {
+    os <- Sys.info()[1]
+    if (os=="Darwin")       content <- read.table(pipe("pbpaste"),  header=header, ...)
+    else if (os=="Windows") content <- read.table(file="clipboard", header=header, ...)
+    else stop("Sorry, only works on Windows and Mac.")
+    if ("package:dplyr" %in% search()) content <- tbl_df(content)
+    return(content)
 }
