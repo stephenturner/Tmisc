591250a977a7969bed89eb1c4255349489ad7257
Stpehen Turner <vustephen@gmail.com>
2016-10-03 13:06:51 -0400

    add several functions from sfirke/janitor

--- R/Tmisc-package.R
+++ R/Tmisc-package.R
@@ -7,3 +7,6 @@
 #' @name Tmisc
 #' @docType package
 NULL
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+## and the "n" that is produced by dplyr::count() in a pipeline
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "n"))
