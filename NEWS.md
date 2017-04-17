90339e9e4e5dac5ed4987dece495dc1a7ad9ca1b
Stephen Turner <vustephen@gmail.com>
2017-04-17 09:47:41 -0400

    add keep top n

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,8 @@
 # Tmisc 0.1.14
 
 - Small updates to enable documentation with [pkgdown](https://github.com/hadley/pkgdown).
-- Inconsequential update to `%nin%`
+- Inconsequential update to `%nin%`.
+- `keep_top_n()` thanks to [Jeroen Janssens](https://gist.github.com/jeroenjanssens/1c628c7e07429e5f0f2245e8598ec8e9).
 
 # Tmisc 0.1.13
 
---
+++ R/keep_top_n.R
@@ -0,0 +1,23 @@
+#' Keep rows from top groups of a column
+#' 
+#' Extract rows belonging to top n groups of a certain column 
+#' 
+#' @author Jeroen Janssens
+#'
+#' @param .data The data frame to operate on
+#' @param col A formula indicating the column to group over
+#' @param n The number of top groups to extract
+#' 
+#' @importFrom dplyr semi_join count_
+#' 
+#' @return A data frame conaining only rows belonging to the top n groups of the column
+#'
+#' @examples
+#' # All the cars
+#' (nrow(mtcars))
+#' # Only those in the top 2 groups based on the # carbs
+#' nrow(keep_top_n(mtcars, ~carb, n=2))
+#' @export
+keep_top_n <- function(.data, col, n = 10) {
+    semi_join(.data, head(count_(.data, col, sort = TRUE), n))
+}
