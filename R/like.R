66a914557634051882cc754c5e148ada8dfa44d1
Stephen Turner <vustephen@gmail.com>
2017-05-17 10:51:44 -0400

    add grapes like nlike

---
+++ R/like.R
@@ -0,0 +1,38 @@
+#' @title  x like y
+#' @description Returns a logical vector of elements of x matching the regex y.
+#' @param x a vector (numeric, character, factor)
+#' @param pattern a vector (numeric, character, factor), matching the mode of x
+#' @return A logical vector with length equal to \code{x} of things in \code{x} that are like \code{y}.
+#' @seealso \code{\link{\%like\%}}, \code{\link{\%nlike\%}}, \code{\link{\%nin\%}}, 
+#' @examples
+#' (Name <- c("Mary","George","Martha"))
+#' Name %in% c("Mary")
+#' Name %like% "^Mar"
+#' Name %nin% c("George")
+#' Name %nlike% "^Mar"
+#' @export
+`%like%` <- function(x, pattern)
+{
+    if (is.factor(x)) {
+        as.integer(x) %in% grep(pattern,levels(x))
+    } else {
+        grepl(pattern,x)
+    }
+}
+
+#' @title  x not like y
+#' @description Returns a logical vector of elements of x not matching the regex y.
+#' @param x a vector (numeric, character, factor)
+#' @param pattern a vector (numeric, character, factor), matching the mode of x
+#' @return A logical vector with length equal to \code{x} of things in \code{x} that aren't like \code{y}.
+#' @seealso \code{\link{\%like\%}}, \code{\link{\%nlike\%}}, \code{\link{\%nin\%}}, 
+#' @examples 
+#' (Name <- c("Mary","George","Martha"))
+#' Name %in% c("Mary")
+#' Name %like% "^Mar"
+#' Name %nin% c("George")
+#' Name %nlike% "^Mar"
+#' @export
+`%nlike%` <- function(x, pattern) {
+    !(x %like% pattern)
+}
