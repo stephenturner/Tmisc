1406b7831a4b673219e13b9feff48c55c126abf3
Stephen Turner <vustephen@gmail.com>
2017-10-10 09:28:04 -0400

    remove gistfo

--- R/addins.R
+++ R/addins.R
@@ -16,35 +16,3 @@ insertInAddin <- function() {
 insertEqual <- function() {
     rstudioapi::insertText(" = ")
 }
-
-#' @title Get it online
-#' @name addins
-#' @description Create a private Github gist containing the active RStudio tab
-#'   and browse to it. File is named: RStudio_<project>_<filename or file_id>.
-#'   file_id is a unique id for untitled files. It does not relate to the
-#'   untitled number. Taken from \url{https://github.com/MilesMcBain/gistfo}.
-#' @rdname addins
-#' @usage NULL
-#' @export
-gistfo <- function(){
-    source_context <- rstudioapi::getSourceEditorContext()
-    if(source_context$path == ''){
-        name <- paste("unititled",source_context$id,sep = "_")
-    }else{
-        name <- tail(unlist(strsplit(x = source_context$path, split = "/")), 1)
-    }
-    project <- rstudioapi::getActiveProject()
-    if(!is.null(project)){
-        project <-  tail(unlist(strsplit(x = project, split = "/")), 1)
-    }else{
-        project <- ""
-    }
-    gist_name <- paste("RStudio",project,name, sep="_")
-    
-    gistr::gist_create(filename = gist_name,
-                       public = FALSE,
-                       browse = TRUE,
-                       code = source_context$contents
-    )
-    invisible(NULL)
-}
