d3c30e3db06addc240c8fbdcf07ea8e3457f5699
Stephen Turner <vustephen@gmail.com>
2019-03-25 14:46:02 -0400

    deprecate

--- R/deseqresult2tbl.R
+++ R/deseqresult2tbl.R
@@ -21,6 +21,7 @@
 #'   
 #' @export
 deseqresult2tbl <- function(deseqresult, colname="ensgene") {
+    .Deprecated("results(..., tidy=TRUE)")
     gene <- baseMean <- padj <- pvalue <- stat <- NULL #gets rid of the note on pkg check
     if (class(deseqresult) != "DESeqResults") stop("Not a DESeqResults object.")
     deseqresult <- as.data.frame(deseqresult)
