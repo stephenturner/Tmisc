2129fd8e82c9a19afb88a40898c6cd846439fbd3
Stephen Turner <vustephen@gmail.com>
2020-09-15 15:00:32 -0400

    fix URLs

--- R/quartet.R
+++ R/quartet.R
@@ -4,7 +4,5 @@
 #' nearly identical linear regression properties, yet appear very different when
 #' graphed.
 #' 
-#' @source Anscombe's Quartet, 
-#'   \url{https://en.wikipedia.org/wiki/Anscombe%5C%27s_quartet}
 #' @format Data frame with columns.
 "quartet"
